691a2dcfb5e7c8fd248fc5de321f7df99b88ccb7
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2019-11-09 19:00:12 +0330

    Create uploader.sh

---
+++ uploader.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#EDIT TOKEN:
+TOKEN="YOUR_TOKEN"
+FILE_NAME="$1"
+#ALSO MAKE SURE THAT CURL, RAR and JQ IS INSTALLED ON YOUR SYSTEM
+upload() {
+	#$1 is filename
+	res=$(curl -X POST "https://bot.sapp.ir/$TOKEN/uploadFile" -H 'content-type: multipart/form-data' -F file=@"$1")
+	local ok
+	ok=$(jq -r .resultMessage <<<"$res")
+	if [[ "$ok" != "OK" ]]; then
+		echo "Error on file $1:"
+		jq .description <<<"$res"
+		delete=false
+		return
+	fi
+	local id
+	id=$(jq -r .fileUrl <<<"$res")
+	echo "https://bot.sapp.ir/$TOKEN/downloadFile/$id" >>"uploaded_files.txt"
+	local fname
+	fname=$(basename "$1")
+	echo "$id:$fname" >>"files_names.txt"
+}
+#Lets start
+rm -rf /tmp/SoroushUploader
+#At first rar the file and split it into a temp directory
+mkdir /tmp/SoroushUploader
+rar a -M0 -v100M /tmp/SoroushUploader/u.rar "$FILE_NAME" # You can also change the chunk size. Max upload size is 100MB
+#Then get the file names and upload eachone to server
+delete=true
+for filename in /tmp/SoroushUploader/*.rar; do
+	while [ "$(jobs | wc -l)" -ge 10 ]; do # Change 10 if needed
+		sleep 5
+	done
+	upload "$filename" &
+done
+while [[ "$(jobs)" =~ "Running" ]]; do
+	sleep 5
+done
+echo "Done uploading files"
+#Delete the files after
+if [ "$delete" = true ]; then
+	rm -rf /tmp/SoroushUploader
+fi
