814cb283d71fd5703c2952168269ac90bfe2372c
Hirbod Behnam <hirbod@localhost.localdomain>
2019-12-08 11:50:09 -0500

    Allow mult-file on soroush uploader Also remove the files that are uploaded And more comments

--- gap_uploader.sh
+++ gap_uploader.sh
@@ -11,7 +11,7 @@ fi
 rm -rf /tmp/GapUploader
 mkdir /tmp/GapUploader
 #Generate the rar command
-rarCommand="rar a -M0 -v400M /tmp/GapUploader/upload.rar"
+rarCommand="rar a -M0 -v400M /tmp/GapUploader/upload.rar" #You can change this command
 for arg in "$@"
 do
     rarCommand+=" \"$arg\""
@@ -19,20 +19,16 @@ done
 #rar the files
 eval "$rarCommand"
 #upload each file
-delete=true
 for filename in /tmp/GapUploader/*.rar; do
     echo "$(tput setaf 2)Uploading file: $filename $(tput sgr 0)"
     res=$(curl -X POST -H "content-type: multipart/form-data" -H "token: $TOKEN" -F file=@"$filename" --write-out "\n%{http_code}" https://api.gap.im/upload)
     readarray -t ary <<<"$res"
-    if [[ ${ary[1]} != 200 ]]; then
+    if [[ ${ary[1]} != 200 ]]; then #Check if the upload was successful
         echo "$(tput setaf 1)Error on uploading file $filename : ${ary[1]} $(tput sgr 0)"
-        delete=false
         continue
     fi
     path=$(jq -r .path <<<"${ary[0]}")
-    echo "$path" >> links.txt
+    echo "$path" >> links.txt #Save the link
+    rm "$filename" #Remove the file if it is uploaded
 done
-#delete files
-if [[ $delete == true ]]; then
-    rm -rf /tmp/GapUploader
-fi
+echo "$(tput setaf 2)Done$(tput sgr 0)"
--- soroush_uploader.sh
+++ soroush_uploader.sh
@@ -5,7 +5,6 @@ if [[ "$#" -lt 1 ]]; then #Check number of arguemnt
     echo "Please pass file names as argument."
     exit 1
 fi
-FILE_NAME="$1"
 #ALSO MAKE SURE THAT CURL, RAR and JQ ARE INSTALLED ON YOUR SYSTEM
 upload() {
 	#$1 is filename
@@ -15,7 +14,6 @@ upload() {
 	if [[ "$ok" != "OK" ]]; then
 		echo "Error on file $1:"
 		jq .description <<<"$res"
-		touch /tmp/SoroushUploader/.nodelete
 		return
 	fi
 	local id
@@ -25,12 +23,18 @@ upload() {
 	fname=$(basename "$1")
 	echo "bot.sapp.ir/$TOKEN/downloadFile/$id:$fname" >>"file_names.txt"
 	echo "IDMan.exe /d https://bot.sapp.ir/$TOKEN/downloadFile/$id /f $fname /a" >> idm.bat
+	rm "$fname"
 }
 #Lets start
 rm -rf /tmp/SoroushUploader
 #At first rar the file and split it into a temp directory
 mkdir /tmp/SoroushUploader
-rar a -M0 -v100M /tmp/SoroushUploader/u.rar "$FILE_NAME" # You can also change the chunk size. Max upload size is 100MB
+rarCommand="rar a -M0 -v100M /tmp/SoroushUploader/u.rar" # You can also change the chunk size. Max upload size is 100MB
+for arg in "$@"
+do
+    rarCommand+=" \"$arg\""
+done
+eval "$rarCommand" #Rar the files
 #Then get the file names and upload eachone to server
 for filename in /tmp/SoroushUploader/*.rar; do
 	while [ "$(jobs | wc -l)" -ge 10 ]; do # Change 10 if needed
@@ -42,5 +46,3 @@ while [[ "$(jobs)" =~ "Running" ]]; do
 	sleep 5
 done
 echo "Done uploading files"
-#Delete the files after
-[ ! -f /tmp/SoroushUploader/.nodelete ] && rm -rf /tmp/SoroushUploader
