11f3e11a73083159e22464ed74ebbed3dcc2c0fa
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2020-04-25 12:10:10 +0430

    Update gap_force_foreign.sh

--- gap_force_foreign.sh
+++ gap_force_foreign.sh
@@ -2,6 +2,7 @@
 # 77.238.120.242: That the server is in Iran
 # 195.201.142.60: That the server is in Germany; You can get this IP with https://www.ipaddressguide.com/ping
 # If you run this file, it forces your server to upload to the Germany server.
+# This is a good idea if your server is blacklisted in Iran
 # You can revert this back by editing /etc/hosts file and removing this line
 # Run this script with bash to set the IP address
 echo "195.201.142.60 api.gap.im" >> /etc/hosts
