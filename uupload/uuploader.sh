1a4cf9738c7a83e33d2d8c7f923e71eee6d0dff9
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2025-03-31 10:30:55 +0330

    Fixed link regex

--- uupload/uuploader.sh
+++ uupload/uuploader.sh
@@ -6,7 +6,7 @@ if [[ "$#" -lt 1 ]]; then
   exit 1
 fi
 #Upload each file
-RESULT_REGEX="<img src=\"css/images/file.png\" style=\"margin-bottom:4px;\" alt=\"([0-9a-zA-Z_.]+)\" />"
+RESULT_REGEX="<img src=\"css/images/file.png\" style=\"margin-bottom:4px;\" alt=\"([0-9a-zA-Z_.\-]+)\" />"
 for filename in "$@"; do
   echo "$(tput setaf 2)Uploading file: $filename $(tput sgr 0)"
   res=$(curl -x socks5://127.0.0.1:10808 -X POST -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0' -H 'Origin: https://uupload.ir' -H 'Connection: keep-alive' -H 'Referer: https://uupload.ir/' -F hash=$HASH -F __userfile[]=@"$filename" -F ittl=86400 https://s6.uupload.ir/sv_process.php)
